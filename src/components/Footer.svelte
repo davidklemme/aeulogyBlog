92fcaa059847158e240b5560e3bb66ab1e2cd33a
David Klemme <david@berlinersoftwareschmiede.de>
2023-03-03 09:22:40 +0100

    fix logo

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,7 +3,7 @@
     <div class="w-full shadow-sm border-b-2" />
     <div>
         <div class="font-mono text-xs m-2 md:m-8 flex flex-col md:flex-row justify-evenly md:items-center">
-            <p>© 2023 <span class="font-extrabold">Aeulogy</span> a brand of Berliner Softwareschmiede. All rights reserved.</p>
+            <p>© 2023 <span class="font-extrabold">Aeulogy</span> a brand of <a href="https://www.berlinersoftwareschmiede.de">Berliner Softwareschmiede</a>. All rights reserved.</p>
             <a href="https://app.termly.io/document/privacy-policy/e95c6189-892b-4939-a0f9-c73ede7a98c7" class="pt-2 md:pt-0">Privacy</a>
             <a href="https://app.termly.io/document/terms-and-conditions/9e515fe4-23fa-4b75-bf26-2c2e1e6f04bb" class="pt-2 md:pt-0">Terms of Service</a>
             <a href="https://app.termly.io/document/disclaimer/8ec07365-7680-4bed-8755-db2cb8141dad" class="pt-2 md:pt-0">Disclaimer</a>
