5d029feb32c28c32d735e31ff8208060f283df85
David Klemme <david@berlinersoftwareschmiede.de>
2023-03-03 14:26:02 +0100

    fix positions

--- src/content/topics.ts
+++ src/content/topics.ts
@@ -7,7 +7,7 @@ export const topics = [
 	},
 	{
 		id: '1',
-		title: 'About eulogies',
+		title: 'About Eulogies',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 	},
