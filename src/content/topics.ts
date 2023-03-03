2dd8619237b5086c87a150c9b731e053c682fabb
David Klemme <david@berlinersoftwareschmiede.de>
2023-03-03 10:26:50 +0100

    add topics

--- src/content/topics.ts
+++ src/content/topics.ts
@@ -13,7 +13,25 @@ export const topics = [
 	},
 	{
 		id: '2',
-		title: 'Help & Support',
+		title: 'Support',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+	},
+	{
+		id: '3',
+		title: 'Finding Inspiration',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+	},
+	{
+		id: '4',
+		title: 'Celebrating Life',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+	},
+	{
+		id: '5',
+		title: 'Collaborating with Others',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 	}
