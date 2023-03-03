23d295aebc543917f23089adee1eda43ae5d24ad
David Klemme <david@berlinersoftwareschmiede.de>
2023-03-03 15:20:02 +0100

    add topic descriptions

--- src/content/topics.ts
+++ src/content/topics.ts
@@ -2,37 +2,42 @@ export const topics = [
 	{
 		id: '0',
 		title: 'Most recent',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+		description: ''
 	},
 	{
 		id: '1',
 		title: 'Writing a eulogy',
 		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+			'Offers tips that focus on creating a eulogy effectively that honors the memory of a loved one..'
 	},
 	{
 		id: '2',
 		title: 'Support',
 		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+			'Resources and tools to support during a difficult time. These articles aim to provide comfort and closure during the grieving process.'
 	},
 	{
 		id: '3',
 		title: 'Finding Inspiration',
 		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+			'Offers practical advice, inspirational stories, and writing prompts to help individuals discover meaningful and uplifting themes and memories to include in their eulogies.'
 	},
 	{
 		id: '4',
 		title: 'Celebrating Life',
 		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+			'Includes articles that highlight the impact and memories of the deceased, providing a comforting and memorable tribute to celebrate their unique stories and contributions.'
 	},
 	{
 		id: '5',
 		title: 'Collaborating with Others',
 		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+			'Insights, tips, and strategies for collaborating with family members, friends, and colleagues to create the best eulogy for your loved one.'
+	},
+	{
+		id: '6',
+		title: 'About Eulogies',
+		description:
+			'Explores the history, significance, and cultural practices surrounding eulogies, providing insights into the traditions and customs associated with honoring and remembering loved ones.'
 	}
 ];
