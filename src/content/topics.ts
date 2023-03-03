cb9d214c3bdec444da87b4e68fd47ef59e4a746d
David Klemme <david@berlinersoftwareschmiede.de>
2023-03-03 15:23:03 +0100

    add topic descriptions

--- src/content/topics.ts
+++ src/content/topics.ts
@@ -4,38 +4,33 @@ export const topics = [
 		title: 'Most recent',
 		description: ''
 	},
+
 	{
 		id: '1',
-		title: 'About Eulogies',
+		title: 'Writing a eulogy',
 		description:
-			'Offers tips that focus on creating a eulogy effectively that honors the memory of a loved one..'
+			'Offers tips that focus on creating a eulogy effectively that honors the memory of a loved one.'
 	},
 	{
 		id: '2',
-		title: 'Writing a eulogy',
+		title: 'Support',
 		description:
 			'Resources and tools to support during a difficult time. These articles aim to provide comfort and closure during the grieving process.'
 	},
 	{
 		id: '3',
-		title: 'Support',
+		title: 'Finding Inspiration',
 		description:
 			'Offers practical advice, inspirational stories, and writing prompts to help individuals discover meaningful and uplifting themes and memories to include in their eulogies.'
 	},
 	{
 		id: '4',
-		title: 'Finding Inspiration',
+		title: 'Celebrating Life',
 		description:
 			'Includes articles that highlight the impact and memories of the deceased, providing a comforting and memorable tribute to celebrate their unique stories and contributions.'
 	},
 	{
 		id: '5',
-		title: 'Celebrating Life',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-	},
-	{
-		id: '6',
 		title: 'Collaborating with Others',
 		description:
 			'Insights, tips, and strategies for collaborating with family members, friends, and colleagues to create the best eulogy for your loved one.'
